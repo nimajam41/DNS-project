907b495c3b9dbc0b93ceb3c1cb492b956e16568a
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-07-14 19:43:07 +0430

    Add ssl

--- classes/const.py
+++ classes/const.py
@@ -2,3 +2,6 @@ pk_bank_byte = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMII
 
 merchant_id = '88ec7410-a926-467e-a254-dbede767d4a9'
 payer_id = '712d29cb-5f3e-46f3-826b-63cbf67c1947'
+
+certs_path = "certs/"
+blockchain_port = 8001
